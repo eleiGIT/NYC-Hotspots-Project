3464a5b31c9b9ee2e9dffa2c11a2fa33ec3bbb35
tjiang1472 <ting.jiang96@myhunter.cuny.edu>
2025-03-03 15:14:41 -0500

    init checkbox filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [providerClick, setProviderClick] = useState(false);
+  const [typeClick, setTypeClick] = useState(false);
+
+  const moreProvider = async () => {
+    if (providerClick) {
+      setProviderClick(false);
+    } else {
+      setProviderClick(true);
+    }
+  };
+
+  const moreType = async () => {
+    if (typeClick) {
+      setTypeClick(false);
+    } else {
+      setTypeClick(true);
+    }
+  };
+
   return (
     <div id="main">
       <input
@@ -12,12 +32,27 @@ function App() {
       <div id="map">
         <div id="filter">
           <div>
-            <button></button>
-            Provider
+            {" "}
+            Filters
+            <br></br>
+            <input
+              type="checkbox"
+              id="provider"
+              name="provider"
+              onClick={moreProvider}
+            ></input>
+            <label for="provider">Provider</label>
+            {providerClick && <div>I GOT CLICKED</div>}
           </div>
           <div>
-            <button></button>
-            Type
+            <input
+              type="checkbox"
+              id="type"
+              name="type"
+              onClick={moreType}
+            ></input>
+            <label for="type">Type</label>
+            {typeClick && <div>I GOT CLICKED</div>}
           </div>
         </div>
         <img src="./test.PNG"></img>
