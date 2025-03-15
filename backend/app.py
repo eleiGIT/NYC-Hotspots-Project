dae016ff867e7bd6cea7a0de23a0566b6a2c7e46
JackHumala <124221447+JackHumala@users.noreply.github.com>
2025-03-15 18:27:06 -0400

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,19 @@ mongo_uri = os.getenv("MONGODB_URI")
 
 app = Flask(__name__)
 
+
+#landing page
+@app.route('/')
+def landing_page():
+    return '''
+        <html>
+            <body>
+                <h1>Landing page for NYC Hotspots.</h1>
+                <p>Click <a href="http://localhost:5000/api/hotspots">here</a> to see all hotspots.</p>
+            <body>
+        <html>
+'''
+
 #connect to my mongo db database3
 try:
     client = MongoClient(mongo_uri, tlsCAFile=certifi.where())
@@ -17,7 +30,6 @@ try:
     print("Connected to MongoDB")
 except Exception as e: 
     print(f"Failed to connect to Mongo DB: {e}")
-
 
 
 #fetches all hotspots on database. WHen user first enters the site
@@ -49,21 +61,21 @@ def get_hotspots_by_zip(zip_code):
 #http://127.0.0.1:5000/api/hotspots/zip/11377?type=Free&provider=Transit%20Wireless
 #http://127.0.0.1:5000/api/hotspots/zip/11235?type=Free
 #http://127.0.0.1:5000/api/hotspots/zip/10018?type=Partner%20Site
+#http://127.0.0.1:5000/api/hotspots/zip/11377?provider=Transit%20Wireless,SPECTRUM&type=Free,Limited%20Free
 
 @app.route('/api/hotspots/zip/<int:zip_code>', methods=['GET'])
 def get_hotspots_by_zip(zip_code):
     try: 
-        provider = request.args.get('provider')
-        type = request.args.get('type')
+        provider = request.args.get('provider', '').split(',')
+        type = request.args.get('type', '').split(',')
 
         query = {"Postcode": zip_code}
-        if provider:
-            query["Provider"] = provider
-        if type:
-            query["Type"] = type
+        if provider and provider[0]:
+            query["Provider"] = {"$in": provider}
+        if type and type[0]:
+            query["Type"] = {"$in": type}
 
         hotspots = list(collection.find(query, {'_id' : 0}))
-
 
         if not hotspots:
             return jsonify({"error": f"No hotspots with these filters found in {zip_code}"}), 404
