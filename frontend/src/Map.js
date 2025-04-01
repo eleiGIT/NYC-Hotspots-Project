a0e0f83f97f0f123ae808a1e3b0088b8e6e84a9b
eleiHunter <72729407+eleiHunter@users.noreply.github.com>
2025-03-31 20:18:05 -0400

    update to pull

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { memo } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { icon, marker } from "leaflet";
+import { divIcon, icon, marker } from "leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
 const position = [40.71427, -74.00597];
 
@@ -10,7 +11,14 @@ const customIcon = new icon({
   iconSize: [38, 38],
 });
 
-const Map = ({ markerData }) => {
+const customClusterIcon = (cluster) => {
+  return new divIcon({
+    html: `<div class="cluster-icon">${cluster.getChildCount()}<div>`,
+    className: "",
+  });
+};
+
+const Map = memo(({ markerData }) => {
   return (
     <MapContainer
       preferCanvas={true}
@@ -26,28 +34,31 @@ const Map = ({ markerData }) => {
 
       {/* slice(0,100) for first 100 objects as too many obj causes lag */}
       {/* {marker.slice(0, 100).map((marker, i) => */}
-      {markerData.map((marker, i) => (
-        <Marker
-          key={i}
-          position={[marker.Latitude, marker.Longitude]}
-          icon={customIcon}
-        >
-          {/*popup markers*/}
-          <Popup>
-            <strong>Location:</strong> {marker.Location}, {marker.Postcode}<br />
-            <strong>Wifi:</strong> {marker.SSID}, {marker.Type} <br />
-            <a
-              href={`https://www.google.com/maps/search/?api=1&query=${marker.Latitude},${marker.Longitude}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Open in Google Maps
-            </a>
-          </Popup>
-        </Marker>
-      ))}
+      <MarkerClusterGroup chunkedLoading iconCreateFunction={customClusterIcon}>
+        {markerData.map((marker, i) => (
+          <Marker
+            key={i}
+            position={[marker.Latitude, marker.Longitude]}
+            icon={customIcon}
+          >
+            {/*popup markers*/}
+            <Popup>
+              <strong>Location:</strong> {marker.Location}, {marker.Postcode}
+              <br />
+              <strong>Wifi:</strong> {marker.SSID}, {marker.Type} <br />
+              <a
+                href={`https://www.google.com/maps/search/?api=1&query=${marker.Latitude},${marker.Longitude}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Open in Google Maps
+              </a>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
-};
+});
 
 export default Map;
